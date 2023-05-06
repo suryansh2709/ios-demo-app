85a9bcf513dd961c39fadf31fac1396e4edac5ec
Suryansh Pal <106135903+suryansh2709@users.noreply.github.com>
2023-05-06 12:22:31 +0530

    Add files via upload

---
+++ app/iOS Demo Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Demo Project
+//
+//  Created by sutharm on 05/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ app/iOS Demo ProjectUITests/iOS_Demo_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS_Demo_ProjectUITestsLaunchTests.swift
+//  iOS Demo ProjectUITests
+//
+//  Created by sutharm on 05/05/23.
+//
+
+import XCTest
+
+final class iOS_Demo_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
